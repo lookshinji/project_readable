cb886e84cacd337eb58444a0b361cc3711f41ff3
Julia Caldas <look.shinji@gmail.com>
2017-09-20 10:44:42 -0300

    Component Files tree created

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,3 +1,8 @@
+// deve listar todas as categorias disponíveis, que devem se conectar a esta categoria
+// deve listar todas as postagens solicitadas pelo voteScore (começando pela pontuação mais alta)
+// deve ter um controle para ordenar o método da lista, incluindo, no mínimo, ordenar por voteScore e ordenar por marcação de hora
+// deve ter um controle para adicionar novas postagens
+
 import React, { Component } from 'react';
 import './style.css';
 
---
+++ frontend/src/components/Category/index.js
@@ -0,0 +1,5 @@
+// idêntica à view padrão, mas filtrada para incluir somente postagens com a categoria selecionada
+// deve listar todas as categorias disponíveis, que devem se conectar a esta categoria
+// deve listar todas as postagens solicitadas pelo voteScore (começando pela pontuação mais alta)
+// deve ter um controle para ordenar o método da lista, incluindo, no mínimo, ordenar por voteScore e ordenar por marcação de hora
+// deve ter um controle para adicionar novas postagens
